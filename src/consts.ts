20fb6eb4bab6ba88811a35d152bf173ac986f744
Neighbouring <neighbouringco@gmail.com>
2024-07-15 01:01:27 -0300

    Initial commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Tokyo Lash';
+export const SITE_DESCRIPTION = 'Extensão de Cílios e Design de Sobrancelhas';
